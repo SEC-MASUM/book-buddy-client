4bc572964c374a23ef83bb7733c7ba9c6c9dbf92
SEC-MASUM <masum.jess42@gmail.com>
2022-04-30 16:58:39 +0600

    feat : add Admin Login SignUp And Home route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Admin from "./Pages/Admin/Admin";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Header from "./Pages/Shared/Header/Header";
+import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
   return (
-    <div className="App text-3xl font-bold underline">
-      <h1>Book Buddy</h1>
+    <div className="bg-gray-100 w-full h-screen">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }

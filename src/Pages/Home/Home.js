1af27b2adf1ee55ee1bef8f6b1a34dfa21125f5c
SEC-MASUM <masum.jess42@gmail.com>
2022-04-30 16:55:44 +0600

    create home Page

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+        
+      <h1>Book Buddy</h1>
+    </div>
+  );
+};
+
+export default Home;

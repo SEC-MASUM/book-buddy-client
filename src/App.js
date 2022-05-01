92644df6c003a87bd8938b16dada3a479b616e32
SEC-MASUM <masum.jess42@gmail.com>
2022-05-01 16:27:30 +0600

    feat : create ManageInventories page and app route in App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Admin from "./Pages/Admin/Admin";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import ManageInventories from "./Pages/ManageInventories/ManageInventories";
 import ResetPassword from "./Pages/ResetPassword/ResetPassword";
 import Header from "./Pages/Shared/Header/Header";
 import SignUp from "./Pages/SignUp/SignUp";
@@ -17,6 +18,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/manageInventories" element={<ManageInventories />} />
       </Routes>
     </div>
   );

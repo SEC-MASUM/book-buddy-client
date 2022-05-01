007d2568a02345fbfcab1428cb67fc5c9869dcfc
SEC-MASUM <masum.jess42@gmail.com>
2022-05-01 16:26:59 +0600

    feat : add manage inventory button in Home Page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Products from "../../Components/Products/Products";
 
 const Home = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-center text-3xl font-bold underline mt-5">Book Buddy</h1>
+      <h1 className="text-center text-3xl font-bold underline mt-5">
+        Book Buddy
+      </h1>
       <Products></Products>
+      <div className="flex justify-center my-10">
+        <Link
+          to="/manageInventories"
+          className=" text-center rounded-lg  bg-amber-400 hover:bg-amber-500 hover:text-white hover:ring  hover:ring-amber-400/50 focus-visible:outline-0 focus:ring  focus:ring-amber-400/50 transition-all duration-400 px-5 py-3"
+        >
+          Manage Inventories
+        </Link>
+      </div>
     </div>
   );
 };

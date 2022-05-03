22c8bf761d1951203ae25973dee994e3ca4ed4ae
SEC-MASUM <masum.jess42@gmail.com>
2022-05-03 20:19:58 +0600

    feat : use conditional rendaring depent on authentic use

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,16 +1,40 @@
+import { signOut } from "firebase/auth";
 import React from "react";
-import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { ImBook } from "react-icons/im";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../Firebase/Firebase.init";
 
 const Header = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth);
+    navigate("/");
+  };
+  console.log(user);
   return (
     <nav className="bg-white px-2 py-3">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <Link to="/">Book Buddy</Link>
+        <Link to="/" className="flex items-center space-x-2">
+          <ImBook className="w-6 h-6 text-green-400" />
+          <span className="text-[#110A57] font-bold"> Book Buddy</span>
+        </Link>
         <ul className="flex space-x-8 items-center text-sm">
           <Link to="/">Home</Link>
           <Link to="/admin">Admin</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          {user ? (
+            <>
+              <Link to="/myItems">My Items</Link>
+              <Link to="/login">{user.displayName}</Link>
+              <button onClick={handleSignOut}>Sign Out</button>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </>
+          )}
         </ul>
       </div>
     </nav>

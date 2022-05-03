73c767596d57405cf49484c35ef9cc5fb4d2f768
SEC-MASUM <masum.jess42@gmail.com>
2022-05-03 20:15:05 +0600

    style : add book icon in navbar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,7 +5,7 @@ import Products from "../../Components/Products/Products";
 const Home = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-center text-3xl font-bold underline mt-5">
+      <h1 className="text-center text-[#110A57] text-3xl font-bold underline mt-5">
         Book Buddy
       </h1>
       <Products></Products>

d42a89290670fd5aea16631f853429f9cbcda5da
SEC-MASUM <masum.jess42@gmail.com>
2022-05-04 23:57:49 +0600

    feat : load MyItem depends on user email when loged in

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,9 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../Firebase/Firebase.init";
+import InventoryDetailsCard from "../InventoryDetails/InventoryDetailsCard/InventoryDetailsCard";
 
 const MyItems = () => {
+  const [products, setProducts] = useState([]);
+  const [state, setState] = useState(false);
+  const [user, loading, error] = useAuthState(auth);
+  
+  useEffect(() => {
+    const url = `http://localhost:5000/mybook/?email=${user.email}`;
+    // const url = `https://book-buddy01.herokuapp.com/book`;
+    (async () => {
+      axios.get(url).then((res) => {
+        // console.log(id);
+        setProducts(res.data.result);
+      });
+    })();
+  }, [state]);
+
   return (
     <div>
       <h1>Here is my Items</h1>
+      {products.map((product) => (
+        <InventoryDetailsCard product={product}>
+          <button>children Button</button>
+        </InventoryDetailsCard>
+      ))}
     </div>
   );
 };

087ae3c841b33c1b30f0e6fa9544e5e00f4fd55c
SEC-MASUM <masum.jess42@gmail.com>
2022-05-04 08:31:03 +0600

    feat : implement  ResetPassword functionality

--- src/Pages/ResetPassword/ResetPassword.js
+++ src/Pages/ResetPassword/ResetPassword.js
@@ -1,54 +1,95 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../Firebase/Firebase.init";
+import Loading from "../Loading/Loading";
 
 const ResetPassword = () => {
-    return (
-      <div className="container mx-auto">
-        <div className="flex justify-center items-center">
-          <div className="w-[300px] flex flex-col items-center  bg-white p-4  my-10 space-y-3 rounded-lg shadow-md">
-            <h1 className="">Reset your password</h1>
-            <form className="w-full">
-              <div className="space-y-3">
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Your email address"
-                  className="w-full p-2 border  rounded-lg border-cyan-400 focus-visible:outline-0 focus:border-cyan-400  focus:ring  focus:ring-cyan-300/50 focus:text-gray-800 transition-all duration-400"
-                />
-                <br />
+  const [email, setEmail] = useState("");
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
 
-                <button className="w-full text-center rounded-lg  bg-cyan-400 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 p-2">
-                  Send reset link
-                </button>
-              </div>
-            </form>
-            <div className="w-full flex justify-center items-center">
-              <Link to="/signup">
-                <small>
-                  Don't have an account yet?{" "}
-                  <span className="text-cyan-400 hover:text-cyan-500 transition-all duration-400 cursor-pointer">
-                    Sign up
-                  </span>
-                </small>
-              </Link>
-            </div>
-            <div className="w-full flex justify-center items-center">
-              <div className="bg-cyan-200 h-[1px] w-full mt-1"></div>
-              <div className="px-2">OR</div>
-              <div className="bg-cyan-200 h-[1px] w-full mt-1"></div>
-            </div>
-            <div className="w-full ">
-              <Link
-                to=""
-                className="block border border-cyan-400 hover:border-cyan-500 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 rounded-full py-2  text-center"
+  if (sending) {
+    return <Loading />;
+  }
+
+  let errorElement;
+  if (error) {
+    if (error?.code === "auth/user-not-found") {
+      error.message = "User not found";
+    } else if (error?.code === "auth/invalid-email") {
+      error.message = "Invalid email";
+    }
+    errorElement = (
+      <span className="text-rose-600 px-2">
+        <small>{error?.message}</small>
+      </span>
+    );
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  
+  const handleSendLink = async (event) => {
+    event.preventDefault();
+    await sendPasswordResetEmail(email);
+    // await sendPasswordResetEmail(email);
+  };
+
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center">
+        <div className="w-[300px] flex flex-col items-center  bg-white p-4  my-10 space-y-3 rounded-lg shadow-md">
+          <h1 className="">Reset your password</h1>
+          <form onSubmit={handleSendLink} className="w-full">
+            <div className="space-y-3">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmailChange}
+                placeholder="Your email address"
+                className="w-full p-2 border  rounded-lg border-cyan-400 focus-visible:outline-0 focus:border-cyan-400  focus:ring  focus:ring-cyan-300/50 focus:text-gray-800 transition-all duration-400"
+                required
+              />
+              <br />
+              {errorElement}
+              <button
+                type="submit"
+                className="w-full text-center rounded-lg  bg-cyan-400 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 p-2"
               >
-                Continue with Google
-              </Link>
+                Send reset link
+              </button>
             </div>
+          </form>
+          <div className="w-full flex justify-center items-center">
+            <Link to="/signup">
+              <small>
+                Don't have an account yet?{" "}
+                <span className="text-cyan-400 hover:text-cyan-500 transition-all duration-400 cursor-pointer">
+                  Sign up
+                </span>
+              </small>
+            </Link>
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <div className="bg-cyan-200 h-[1px] w-full mt-1"></div>
+            <div className="px-2">OR</div>
+            <div className="bg-cyan-200 h-[1px] w-full mt-1"></div>
+          </div>
+          <div className="w-full ">
+            <Link
+              to=""
+              className="block border border-cyan-400 hover:border-cyan-500 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 rounded-full py-2  text-center"
+            >
+              Continue with Google
+            </Link>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ResetPassword;

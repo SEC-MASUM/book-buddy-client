56c5cfd60bfeae61398e28b11c7897e07aee3963
SEC-MASUM <masum.jess42@gmail.com>
2022-05-04 08:30:05 +0600

    fix : remove warning by replacing class by className

--- src/Pages/Loading/Loading.js
+++ src/Pages/Loading/Loading.js
@@ -2,8 +2,8 @@ import React from "react";
 import loading from "../../Assets/images/loading.gif";
 const Loading = () => {
   return (
-    <div class="grid min-h-screen place-content-center">
-      <div class="">
+    <div className="grid min-h-screen place-content-center">
+      <div className="">
         <img src={loading} alt="" />
       </div>
     </div>

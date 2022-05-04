65e2c4afe8134c6236e42d1e6386be27c9e4bb4b
SEC-MASUM <masum.jess42@gmail.com>
2022-05-04 09:56:29 +0600

    feat : implement SignUp page functionality

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,17 +1,75 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import {
+  useCreateUserWithEmailAndPassword,
+  useSignInWithGoogle,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import auth from "../../Firebase/Firebase.init";
+import Loading from "../Loading/Loading";
 const SignUp = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+
+  let errorElement;
+
+  const handleGoogleLogin = async () => {
+    await signInWithGoogle();
+  };
+
+  const handleCreateUser = async (event) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      await createUserWithEmailAndPassword(email, password);
+      await updateProfile({ displayName });
+    } else {
+      toast("Confirm your Password");
+    }
+  };
+
+  if (googleError || error || updateError) {
+    // console.log(googleError?.message || error?.message);
+    errorElement = (
+      <p className="text-rose-600">
+        <small>
+          {googleError?.message || error?.message || updateError?.message}
+        </small>
+      </p>
+    );
+  }
+  if (googleLoading || loading || updating) {
+    return <Loading />;
+  }
+  if (googleUser || user) {
+    navigate(from, { replace: true });
+  }
+
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center">
         <div className="w-[300px] flex flex-col items-center  bg-white p-4  my-10 space-y-3 rounded-lg shadow-md">
           <h1 className="">Create your profile</h1>
-          <form className="w-full">
+          <form onSubmit={handleCreateUser} className="w-full">
             <div className="space-y-3">
               <input
                 type="text"
                 name="name"
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
                 placeholder="Name"
                 className="w-full p-2 border  rounded-lg border-cyan-400 focus-visible:outline-0 focus:border-cyan-400  focus:ring  focus:ring-cyan-300/50 focus:text-gray-800 transition-all duration-400"
               />
@@ -19,6 +77,8 @@ const SignUp = () => {
               <input
                 type="email"
                 name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
                 className="w-full p-2 border  rounded-lg border-cyan-400 focus-visible:outline-0 focus:border-cyan-400  focus:ring  focus:ring-cyan-300/50 focus:text-gray-800 transition-all duration-400"
               />
@@ -26,6 +86,8 @@ const SignUp = () => {
               <input
                 type="password"
                 name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
                 className="w-full p-2 border rounded-lg  border-cyan-400 focus-visible:outline-0 focus:border-cyan-400   focus:ring  focus:ring-cyan-300/50 focus:text-gray-800 transition-all duration-400"
               />
@@ -33,15 +95,21 @@ const SignUp = () => {
               <input
                 type="password"
                 name="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
                 placeholder="Confirm Password"
                 className="w-full p-2 border rounded-lg  border-cyan-400 focus-visible:outline-0 focus:border-cyan-400   focus:ring  focus:ring-cyan-300/50 focus:text-gray-800 transition-all duration-400"
               />
               <br />
 
-              <button className="w-full text-center rounded-lg  bg-cyan-400 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 p-2">
+              <button
+                type="submit"
+                className="w-full text-center rounded-lg  bg-cyan-400 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 p-2"
+              >
                 Sign Up
               </button>
             </div>
+            {errorElement}
           </form>
           <div className="w-full flex justify-center items-center">
             <Link to="/login">
@@ -59,12 +127,12 @@ const SignUp = () => {
             <div className="bg-cyan-200 h-[1px] w-full mt-1"></div>
           </div>
           <div className="w-full ">
-            <Link
-              to=""
-              className="block border border-cyan-400 hover:border-cyan-500 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 rounded-full py-2  text-center"
+            <button
+              onClick={handleGoogleLogin}
+              className="w-full block border border-cyan-400 hover:border-cyan-500 hover:bg-cyan-500 hover:text-white hover:ring  hover:ring-cyan-400/50 focus-visible:outline-0 focus:ring  focus:ring-cyan-400/50 transition-all duration-400 rounded-full py-2  text-center"
             >
               Continue with Google
-            </Link>
+            </button>
           </div>
         </div>
       </div>

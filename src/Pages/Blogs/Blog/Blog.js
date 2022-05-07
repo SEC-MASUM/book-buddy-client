0b2be17c77a2ce2be4a2dce64499b07f9d525702
SEC-MASUM <masum.jess42@gmail.com>
2022-05-07 19:25:54 +0600

    add 4 quetions answer in Blogs Page

--- src/Pages/Blogs/Blog/Blog.js
+++ src/Pages/Blogs/Blog/Blog.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Blog = () => {
-  return <div>Blog</div>;
+const Blog = ({ blog }) => {
+  console.log(blog.body);
+
+  return (
+    <div>
+      <div className="max-w-lg mx-auto">
+        <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5">
+          <Link to="">
+            <img className="h-56 w-full rounded-t-lg" src={blog.image} alt="" />
+          </Link>
+          <div className="p-5">
+            <Link to="">
+              <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+                {blog.title}
+              </h5>
+            </Link>
+            <p
+              className="font-normal text-gray-700 mb-3"
+              dangerouslySetInnerHTML={{ __html: blog.body }}
+            ></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,10 +1,24 @@
-import React from "react";
-import Blog from "./Blog/Blog";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Blog from "../Blogs/Blog/Blog";
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("blogs.json");
+      setBlogs(data);
+    })();
+  }, []);
   return (
     <div className="container mx-auto">
-      <Blog />
+      <div className="container mx-auto ">
+        <div className=" grid grid-cols-3 gap-4 py-10">
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog}></Blog>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };

a865d680b58226c1ec79e0f0734a839913dcc6da
SEC-MASUM <masum.jess42@gmail.com>
2022-05-08 22:28:54 +0600

    feat : add conditional rendering

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -12,8 +12,7 @@ const MyItems = () => {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [state, setState] = useState(false);
-  const [user, userLoading, error] = useAuthState(auth);
-
+  const [user] = useAuthState(auth);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -30,7 +29,6 @@ const MyItems = () => {
 
         setProducts(data.result);
       } catch (error) {
-        // console.log(error);
         if (error.response.status === 401 || error.response.status === 403) {
           signOut(auth);
           navigate("/login");
@@ -47,41 +45,32 @@ const MyItems = () => {
     setState(!state);
   };
 
-  // const handleDelete = (id) => {
-  //   const url = `https://book-buddy01.herokuapp.com/book/${id}`;
-  //   // const url = `http://localhost:5000/book/${id}`;
-
-  //   (async () => {
-  //     await axios.delete(url).then((res) => {
-  //       // console.log(res.data.message);
-  //       toast(res.data.message);
-  //       setState(!state);
-  //     });
-  //   })();
-  // };
-
   return (
     <div className="container mx-auto">
       <div className="min-w-[300px] w-full p-3 my-10">
         <h1 className="text-center text-[#110A57] text-3xl font-bold underline mb-5">
           My Items
         </h1>
-        <div>
-          {products.map((product) => (
-            <InventoryDetailsCard
-              key={product._id}
-              product={product}
-              handleState={handleState}
-            >
-              <ConfirmDeleteModal
+        {products.length !== 0 ? (
+          <div>
+            {products.map((product) => (
+              <InventoryDetailsCard
+                key={product._id}
+                product={product}
                 handleState={handleState}
-                productId={product._id}
-                loading={loading}
-                setLoading={setLoading}
-              />
-            </InventoryDetailsCard>
-          ))}
-        </div>
+              >
+                <ConfirmDeleteModal
+                  handleState={handleState}
+                  productId={product._id}
+                  loading={loading}
+                  setLoading={setLoading}
+                />
+              </InventoryDetailsCard>
+            ))}
+          </div>
+        ) : (
+          <h2 className="text-2xl font-semibold text-cyan-400 flex justify-center items-center h-72">You don't have added any book</h2>
+        )}
       </div>
     </div>
   );

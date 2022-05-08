c591f2edbae487090f4ca0a53e08426bc0950610
SEC-MASUM <masum.jess42@gmail.com>
2022-05-08 18:08:00 +0600

    refactor: change banner image source

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,12 @@
 import React from "react";
-import banner from "../../Assets/images/image.jpg";
 const Banner = () => {
   return (
     <div className="min-w-[300px] h-screen relative">
-      <img src={banner} alt="" className="w-full h-full object-cover " />
+      <img
+        src="https://i.ibb.co/wWZctCm/image.jpg"
+        alt=""
+        className="w-full h-full object-cover "
+      />
       <div className="w-full h-full flex justify-center items-center text-6xl font-extrabold text-center  text-white underline bg-black/50 absolute top-0">
         <h1 className="">Book Buddy</h1>
       </div>
